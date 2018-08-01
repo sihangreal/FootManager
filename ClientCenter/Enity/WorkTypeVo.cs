335c380997fc45bfb540e1b3df5dbf913dd494b7
chensihang <137238384@qq.com>
2018-08-01 23:51:57 +0800

    删除

---
+++ ClientCenter/Enity/WorkTypeVo.cs
@@ -0,0 +1,20 @@
+﻿using ClientCenter.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCenter.Enity
+{
+    [DataAttr("WorkType")]
+    public class WorkTypeVo
+    {
+        [DataAttr(false,true)]
+        [ColumnAttr("编号",false)]
+        public int TypeId { get; set; }
+        [DataAttr(true)]
+        [ColumnAttr("类别",true)]
+        public string TypeName { get; set; }
+    }
+}
